f3d1249f5ec762b876b09b16872191651858a3a2
ANIEBIERE <justinaumoh@gmail.com>
2023-10-02 16:27:15 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+import getListStudents from "./0-get_list_students.js";
+ 
+[
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+]
