9b35a95202b01101bcac40ce6ed096f78c5b0897
ANIEBIERE <justinaumoh@gmail.com>
2023-10-03 19:38:28 +0000

    Data Manipulation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,11 +1,3 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentsByLocation from "./2-get_students_by_loc.js";
-
-const students = getListStudents();
-
-console.log(getStudentsByLocation(students, 'San Francisco'))
-
-[
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-]
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location.localeCompare(city) === 0);
+}
