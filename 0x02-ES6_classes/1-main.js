52af249774388e98384d13d25c3c5729b7fbff38
ANIEBIERE <justinaumoh@gmail.come>
2023-10-09 14:27:33 +0000

    1-main.js

---
+++ 0x02-ES6_classes/1-main.js
@@ -0,0 +1,8 @@
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
+[
+  ClassRoom { _maxStudentsSize: 19 },
+  ClassRoom { _maxStudentsSize: 20 },
+  ClassRoom { _maxStudentsSize: 34 }
+]
